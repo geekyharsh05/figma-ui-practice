7d812f9beae43c9ad6ed27140722e11a6401cd00
geekyharsh05 <hp424805@gmail.com>
2024-03-07 15:06:17 +0530

    feat: add source files

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import NavbarSection from "./components/navbar/page";
+import HeroSection from "./components/hero/page";
+import FeaturesSection from "./components/features/page";
+import BenefitsSection from "./components/benefits/page";
+import PricingSection from "./components/pricing/page";
+import DiscountSection from "./components/discount/page";
+import FooterSection from "./components/footer/page";
+
+export default function Home() {
+  return (
+    <>
+      <NavbarSection />
+      <HeroSection />
+      <FeaturesSection />
+      <BenefitsSection />
+      <PricingSection />
+      <DiscountSection />
+      <FooterSection />
+    </>
+  );
+}
