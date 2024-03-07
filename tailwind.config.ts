739c65ee3ddba0efbe4988321b1316dee5b784ac
geekyharsh05 <hp424805@gmail.com>
2024-03-07 15:05:42 +0530

    chore: project setup

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        JostB700 : ['Jost-B'],
+        JostR400 : ['Jost-R'],
+        JostM500 : ['Jost-M'],
+      }, 
+      screens : {
+        xs: '475px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        xxl: '1440px',
+      },
+      zIndex :{
+        '100' : '100',
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
